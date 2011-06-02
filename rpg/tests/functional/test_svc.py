c358c13912b5d02a22159d7775178c44ac7ea24e
Jonathan Gardner <jgardner@jonathangardner.net>
2011-06-02 16:39:01 -0700

    Added test_svc.py

---
+++ rpg/tests/functional/test_svc.py
@@ -0,0 +1,7 @@
+from rpg.tests import *
+
+class TestSvcController(TestController):
+
+    def test_index(self):
+        response = self.app.get(url(controller='svc', action='index'))
+        # Test response...
